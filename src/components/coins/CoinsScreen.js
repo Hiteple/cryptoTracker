effbe78e9e688507c71e94c52b6bd0399feb9db2
hiteple <hiteplegold@gmail.com>
2020-11-25 02:58:23 -0300

    Adding TextInput

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -2,28 +2,45 @@ import React, {Component} from 'react';
 import Http from 'cryptoTracker/src/libs/http';
 import {View, Text, FlatList, StyleSheet} from 'react-native';
 import CoinsItem from './CoinsItem';
+import CoinsSearch from './CoinsSearch';
 import colors from 'cryptoTracker/src/resources/colors';
 
 class CoinsScreen extends Component {
   state = {
     coins: [],
+    allCoins: []
   };
 
-  componentDidMount = async () => {
-    const res = await Http.instance.get(
+  componentDidMount = () => {
+    this.getCoins();
+  };
+
+  getCoins = async () => {
+   const res = await Http.instance.get(
       'https://api.coinlore.net/api/tickers/',
     );
-    this.setState({coins: res.data});
-  };
+    this.setState({coins: res.data, allCoins: res.data});
+  }
 
   handlePress = (coin) => {
     this.props.navigation.navigate('CoinsDetail', {coinObj: coin});
   };
 
+  handleSearch = (query) => {
+      const {allCoins} = this.state;
+
+      const coinsFiltered = allCoins.filter(coin => {
+         return coin.name.toLowerCase().includes(query.toLowerCase()) || coin.symbol.toLowerCase().includes(query.toLowerCase())
+      });
+
+      this.setState({coins: coinsFiltered});
+  }
+
   render() {
     const {coins} = this.state;
     return (
       <View style={styles.container}>
+      <CoinsSearch onChange={this.handleSearch} />
         <FlatList
           data={coins}
           renderItem={({item}) => (
